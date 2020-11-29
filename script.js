6b9a1225e0be8391404769573cf7fb2a4627d939
lai-github <64319809+lai-github@users.noreply.github.com>
2020-11-29 11:21:37 +0800

    test002
    22222

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 // 用一個物件陣列來放照片的名字跟來源，不僅好管理，還可以搭配 for 迴圈使用
 var images = [
-    { 
-        name: "青蘋果",
+    {
+        name: "001青蘋果",
         photoURL: "images/fruit-1.jpg"
-    }, 
-    { 
+    },
+    {
         name: "草莓",
         photoURL: "images/fruit-2.jpg"
-    }, 
-    { 
+    },
+    {
         name: "橘子",
         photoURL: "images/fruit-3.jpg"
-    }, 
-    { 
+    },
+    {
         name: "西瓜",
         photoURL: "images/fruit-4.jpg"
-    }, 
-    { 
+    },
+    {
         name: "火龍果",
         photoURL: "images/fruit-5.jpg"
-    }, 
-    { 
+    },
+    {
         name: "莓果",
         photoURL: "images/fruit-6.jpg"
-    }, 
-    { 
+    },
+    {
         name: "檸檬",
         photoURL: "images/fruit-7.jpg"
-    }, 
-    { 
+    },
+    {
         name: "百香果",
         photoURL: "images/fruit-8.jpg"
     }
@@ -54,9 +54,9 @@ document.querySelector("#big").addEventListener("click", closeLightBox);
 
 // 由資料決定畫面: 文字不再寫死在 .html，而是透過 javascript 產生畫面
 var li = "";
-for(var i=1; i<=8; i++) {
+for (var i = 1; i <= 8; i++) {
     li += `<li>
-                <img id="fruit-${i}" src="${images[i-1].photoURL}" name="${images[i-1].name}">
+                <img id="fruit-${i}" src="${images[i - 1].photoURL}" name="${images[i - 1].name}">
             </li>`;
 }
 
@@ -64,6 +64,6 @@ for(var i=1; i<=8; i++) {
 document.querySelector("#box").innerHTML = li;
 
 // 綁定事件: 滑鼠點擊小圖就去做這件事 openLightBox
-for(var i=1; i<=8; i++) {
+for (var i = 1; i <= 8; i++) {
     document.querySelector(`#fruit-${i}`).addEventListener("click", openLightBox);
 }
